d5282244207a5d4aac64d351ec5d11cc3b9016e7
albert <albert@e691977d-1c04-0410-b8d3-af1ef8413551>
2006-07-12 08:34:05 +0000

    git-svn-id: https://localhost/svn/renamer/trunk@73 e691977d-1c04-0410-b8d3-af1ef8413551

--- FileNameOperations/RegExpPath.cs
+++ FileNameOperations/RegExpPath.cs
@@ -6,7 +6,7 @@ using System.Text.RegularExpressions;
 namespace RenamerNG.FileNameOperations
 {
 	/// <summary>
-	/// Summary description for RegExp.
+	/// Summary description for RegExpPath.
 	/// </summary>
 	[Serializable()]
 	public class RegExpPath : Operation
