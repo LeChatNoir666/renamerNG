94dfb1e9138aa6297273cd3b4fbff813693b509a
Administrator <Administrator@e691977d-1c04-0410-b8d3-af1ef8413551>
2010-02-22 16:42:01 +0000

    Adjusted dialog controls and size.
    git-svn-id: https://localhost/svn/renamer/trunk@100 e691977d-1c04-0410-b8d3-af1ef8413551

--- FrmAbout.cs
+++ FrmAbout.cs
@@ -53,99 +53,103 @@ namespace RenamerNG
 		/// </summary>
 		private void InitializeComponent()
 		{
-			System.Resources.ResourceManager resources = new System.Resources.ResourceManager(typeof(FrmAbout));
-			this.btOk = new System.Windows.Forms.Button();
-			this.lblVersion = new System.Windows.Forms.Label();
-			this.linkHomepage = new System.Windows.Forms.LinkLabel();
-			this.label2 = new System.Windows.Forms.Label();
-			this.label3 = new System.Windows.Forms.Label();
-			this.pictureBox1 = new System.Windows.Forms.PictureBox();
-			this.label1 = new System.Windows.Forms.Label();
-			this.SuspendLayout();
-			// 
-			// btOk
-			// 
-			this.btOk.DialogResult = System.Windows.Forms.DialogResult.OK;
-			this.btOk.Location = new System.Drawing.Point(96, 192);
-			this.btOk.Name = "btOk";
-			this.btOk.TabIndex = 0;
-			this.btOk.Text = "Ok";
-			// 
-			// lblVersion
-			// 
-			this.lblVersion.Location = new System.Drawing.Point(24, 64);
-			this.lblVersion.Name = "lblVersion";
-			this.lblVersion.Size = new System.Drawing.Size(220, 20);
-			this.lblVersion.TabIndex = 1;
-			this.lblVersion.Text = "Version: ";
-			this.lblVersion.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-			// 
-			// linkHomepage
-			// 
-			this.linkHomepage.Location = new System.Drawing.Point(24, 152);
-			this.linkHomepage.Name = "linkHomepage";
-			this.linkHomepage.Size = new System.Drawing.Size(220, 20);
-			this.linkHomepage.TabIndex = 2;
-			this.linkHomepage.TabStop = true;
-			this.linkHomepage.Text = "http:\\\\www.albert.nu\\programs\\renamerng\\";
-			this.linkHomepage.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkHomepage_LinkClicked);
-			// 
-			// label2
-			// 
-			this.label2.Location = new System.Drawing.Point(24, 104);
-			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(220, 19);
-			this.label2.TabIndex = 3;
-			this.label2.Text = "Written by: Albert Bertilsson";
-			this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-			// 
-			// label3
-			// 
-			this.label3.Location = new System.Drawing.Point(24, 136);
-			this.label3.Name = "label3";
-			this.label3.Size = new System.Drawing.Size(220, 20);
-			this.label3.TabIndex = 4;
-			this.label3.Text = "Home page:";
-			// 
-			// pictureBox1
-			// 
-			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-			this.pictureBox1.Location = new System.Drawing.Point(16, 16);
-			this.pictureBox1.Name = "pictureBox1";
-			this.pictureBox1.Size = new System.Drawing.Size(33, 33);
-			this.pictureBox1.TabIndex = 5;
-			this.pictureBox1.TabStop = false;
-			// 
-			// label1
-			// 
-			this.label1.Location = new System.Drawing.Point(72, 24);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(128, 20);
-			this.label1.TabIndex = 6;
-			this.label1.Text = "Renamer NG";
-			this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-			// 
-			// FrmAbout
-			// 
-			this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
-			this.ClientSize = new System.Drawing.Size(266, 230);
-			this.Controls.Add(this.label1);
-			this.Controls.Add(this.pictureBox1);
-			this.Controls.Add(this.label3);
-			this.Controls.Add(this.label2);
-			this.Controls.Add(this.linkHomepage);
-			this.Controls.Add(this.lblVersion);
-			this.Controls.Add(this.btOk);
-			this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((System.Byte)(0)));
-			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-			this.MaximizeBox = false;
-			this.MinimizeBox = false;
-			this.Name = "FrmAbout";
-			this.ShowInTaskbar = false;
-			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-			this.Text = "About";
-			this.Load += new System.EventHandler(this.FrmAbout_Load);
-			this.ResumeLayout(false);
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmAbout));
+            this.btOk = new System.Windows.Forms.Button();
+            this.lblVersion = new System.Windows.Forms.Label();
+            this.linkHomepage = new System.Windows.Forms.LinkLabel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btOk
+            // 
+            this.btOk.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btOk.Location = new System.Drawing.Point(96, 204);
+            this.btOk.Name = "btOk";
+            this.btOk.Size = new System.Drawing.Size(75, 23);
+            this.btOk.TabIndex = 0;
+            this.btOk.Text = "Ok";
+            // 
+            // lblVersion
+            // 
+            this.lblVersion.Location = new System.Drawing.Point(24, 64);
+            this.lblVersion.Name = "lblVersion";
+            this.lblVersion.Size = new System.Drawing.Size(220, 20);
+            this.lblVersion.TabIndex = 1;
+            this.lblVersion.Text = "Version: ";
+            this.lblVersion.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // linkHomepage
+            // 
+            this.linkHomepage.Location = new System.Drawing.Point(24, 152);
+            this.linkHomepage.Name = "linkHomepage";
+            this.linkHomepage.Size = new System.Drawing.Size(220, 22);
+            this.linkHomepage.TabIndex = 2;
+            this.linkHomepage.TabStop = true;
+            this.linkHomepage.Text = "http:\\\\www.albert.nu\\programs\\renamerng\\";
+            this.linkHomepage.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.linkHomepage.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkHomepage_LinkClicked);
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(24, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(220, 19);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Written by: Albert Bertilsson";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(24, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(220, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Home page:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(16, 16);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(33, 33);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(72, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Renamer NG";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // FrmAbout
+            // 
+            this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
+            this.ClientSize = new System.Drawing.Size(266, 239);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.linkHomepage);
+            this.Controls.Add(this.lblVersion);
+            this.Controls.Add(this.btOk);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmAbout";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "About";
+            this.Load += new System.EventHandler(this.FrmAbout_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
 
 		}
 		#endregion
